d02ffdb06bd0c5bf9f4a56ea85286bd35db8ea15
ict <ict@R1-03.local>
2012-08-30 21:52:00 +0900

    114まで

--- gen/sample/application/calculator/R.java
+++ gen/sample/application/calculator/R.java
@@ -54,6 +54,7 @@ public final class R {
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_calculator=0x7f040003;
+        public static final int toast_div_by_zero=0x7f040004;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
--- src/sample/application/calculator/CalculatorActivity.java
+++ src/sample/application/calculator/CalculatorActivity.java
@@ -1,5 +1,6 @@
 package sample.application.calculator;
 
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
 import android.os.Bundle;
@@ -9,9 +10,14 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CalculatorActivity extends Activity {
 	
+    public String sNum1 = new String();
+    public String strTemp = "";
+    public String strResult = "0";
+    public Integer operator = 0;
 	
 
     @Override
@@ -26,20 +32,7 @@ public class CalculatorActivity extends Activity {
         return true;
     }
     
-    /**
-     * 数字が押されたとき（自作）
-     *
-    public void numKeyOnClick(View v){
-    	Button button = (Button)v;
-    	Log.d("[buttonのtext]", button.getText().toString());			//押されたボタンを取得
-    	
-    	TextView tv = (TextView) this.findViewById(R.id.displayPanel);
-    	
-//    	Log.d("[tvのtext]", tv.getText().toString());					//ディスプレイの表示を取得
-    	
-    	tv.setText(tv.getText().toString() + button.getText().toString());
 
-    }*/
     
     /**
      * 数字が押されたとき（テキスト）
@@ -64,6 +57,9 @@ public class CalculatorActivity extends Activity {
 	    this.showNumber(this.strTemp);
     }
     
+    /**
+     * ディスプレイに表示するメソッド
+     */
     private void showNumber(String strNum) {
     	
     	DecimalFormat form = new DecimalFormat("#,##0");
@@ -88,16 +84,88 @@ public class CalculatorActivity extends Activity {
     	((TextView)findViewById(R.id.displayPanel)).setText(fText);
 	}
     
+    /**
+     * +,-,*,/,= が押されたとき
+     */
     public void operatorKeyOnClick(View v){
     	
+    	if(operator != 0){
+    		if(this.strTemp.length() > 0){
+    			this.strResult = this.doCalc();
+    			this.showNumber(this.strResult);
+    		}
+    	}else{
+    		if(this.strTemp.length() > 0){
+    			this.strResult = this.strTemp;
+    		}
+    	}
+    	
+    	this.strTemp = "";
+    	
+    	if(v.getId() == R.id.keypadEq){
+    		this.operator = 0;
+    	}else{
+    		this.operator = v.getId();
+    	}
     }
     
-    
-    
+    /**
+     * 計算
+     */
+	private String doCalc() {
+		BigDecimal bd1 = new BigDecimal(strResult);
+		BigDecimal bd2 = new BigDecimal(strTemp);
+		BigDecimal result = BigDecimal.ZERO;
+		
+		switch(this.operator){
+		case R.id.keypadAdd:
+			result = bd1.add(bd2);
+			break;
+		case R.id.keypadSub:
+			result = bd1.subtract(bd2);
+			break;
+		case R.id.keypadMulti:
+			result = bd1.multiply(bd2);
+			break;
+		case R.id.keypadDiv:
+			if(!bd2.equals(BigDecimal.ZERO)){
+				result = bd1.divide(bd2, 12, 3);
+			}else{
+				Toast toast = Toast.makeText(this, R.string.toast_div_by_zero, 1000);
+				toast.show();
+			}
+			break;
+		}
+		
+		if(result.toString().indexOf(".") >= 0){
+			return result.toString().replaceAll("¥¥.0+$|0+$", "");
+		}else{
+			return result.toString();
+		}
+	}
+	
+	
+	
+	
+	
+	
 
+    /**
+     * 数字が押されたとき（自作）
+     *
+    public void numKeyOnClick(View v){
+    	Button button = (Button)v;
+    	Log.d("[buttonのtext]", button.getText().toString());			//押されたボタンを取得
+    	
+    	TextView tv = (TextView) this.findViewById(R.id.displayPanel);
+    	
+//    	Log.d("[tvのtext]", tv.getText().toString());					//ディスプレイの表示を取得
+    	
+    	tv.setText(tv.getText().toString() + button.getText().toString());
+    }
 	/**
      * +が押されたとき
-     */
+     *
     Integer num1,num2;
     public void addKeyOnClick(View v){
     	Button button = (Button)v;
@@ -109,10 +177,9 @@ public class CalculatorActivity extends Activity {
     	
     	tv.setText("0");
     }
-    
     /**
      * =が押されたとき
-     */
+     *
     public void equalKeyOnClick(View v){
     	Button button = (Button)v;
     	Log.d("[buttonのtext]", button.getText().toString());			//押されたボタンを取得
@@ -123,9 +190,6 @@ public class CalculatorActivity extends Activity {
     	
     	this.sNum1 = Integer.toString(num1 + num2);
     	tv.setText(this.sNum1);
-    }
-    
-    public String sNum1 = new String();
-    public String strTemp = "";
+    }*/
     
 }
